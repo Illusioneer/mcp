2af987c210fdf2a2a921678c2a0e731a5bbf2dfb
Illusioneer <bryano@Phoenix.localnet>
2013-03-21 15:05:42 -0400

    Removed old alerts scaffold

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -98,8 +98,9 @@ class AlertsController < ApplicationController
   def alert_check
     
     Alert.all.each do |alert|
-      alertcheck = Servicestatus.where(:host_name => alert.host).where(:service_description => 'HTTP').where(:current_state => 1 ... 9).where(:nagiostimeid => (alert.trigger.to_i  * 5+240).minutes.ago ... Time.now).count%>
+      alertcheck = Servicestatus.where(:host_name => alert.host).where(:service_description => 'HTTP').where(:current_state => 1 ... 9).where(:nagiostimeid => (alert.trigger.to_i  * 5+240).minutes.ago ... Time.now).count
       AlertMailer.notification(alert) unless alert.trigger >= alertcheck
+      logger.info "Sending email for #{alert.host}"
     end  
   end
   # DELETE /alerts/1
