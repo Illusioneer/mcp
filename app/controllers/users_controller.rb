2f4984b9258a6100fd720f9b3e4650ab8e9f936e
Illusioneer <khillton@gmail.com>
2013-04-01 13:07:19 -0400

    Adding jquery functionality.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -77,8 +77,8 @@ class UsersController < ApplicationController
     @user.destroy
 
     respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
+    #  format.html { redirect_to users_url }
+    #  format.json { head :no_content }
       format.js   { render :nothing => true }
     end
   end
