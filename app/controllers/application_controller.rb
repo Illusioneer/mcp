803cadf46ed0453c2f7c72bd544a83686efe1c8e
Illusioneer <khillton@gmail.com>
2013-04-05 13:16:46 -0400

    Adding the session controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authorize
-
-  #delegate :allow?, to: :current_permission
-  #helper_method :allow?
 
   private
 
@@ -11,14 +7,4 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
-
-  def current_permission
-    @current_permission ||= Permission.new(current_user)
-  end
-
-  def authorize
-    if !current_permission.allow?(params[:controller], params[:action])
-      redirect_to root_url, alert: "Not authorized."
-    end
-  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,12 @@
 class SessionsController < ApplicationController
-  def new
-  end
-
   def create
     user = User.find_by_name(params[:name])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
-    else
-      flash.now.alert = "Invalid email or password"
-      render "new"
-    end
+    session[:user_id] = user.id
+    redirect_to root_url, notice: "Signed in!"
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Logged out!"
+    redirect_to root_url, notice: "Signed out!"
   end
 end
