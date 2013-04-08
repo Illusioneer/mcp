a60a9db8f50b3f50571335a88c31eb3c4b6fc7ee
Illusioneer <khillton@gmail.com>
2013-04-08 13:04:20 -0400

    Update to user login.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    redirect_to 'login' if @session.nil?
+    redirect_to '/login' if session[:user_id] == nil
+    logger.info("App level response: #{session.inspect}")
+    return session
   end
+
   helper_method :current_user
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,13 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_name(params[:name])
-    @session =  user if user.password == params[:password]
+    session[:user_id] =  user.id if user.password == params[:password]
+    logger.info("Login level response: #{session.inspect}")
     redirect_to root_url, notice: "Signed in!"
   end
 
   def destroy
-    @session = nil
+    session[:user_id] = nil
     redirect_to root_url, notice: "Signed out!"
   end
 end
